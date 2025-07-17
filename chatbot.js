56f29027a6201fb81ad01fda7341150623c902a4
om3r2331 <oldmenlikeme@outlook.com>
2025-07-17 16:22:39 +0400

    Add files via upload

--- chatbot.js
+++ chatbot.js
@@ -1,2 +1,45 @@
+const messages = []
 
 
+document.querySelector('.chat-input').addEventListener('submit', function (event){
+    event.preventDefault();
+
+    const input = document.getElementById('input').value
+    document.getElementById('input').value = '';
+    messages.push({
+           role: 'user',
+           content: input,
+           
+
+    })
+
+    displayMessage(input, 'U');
+
+
+    fetch('https://gpt-gateway.vercel.app/chat', {
+        method: 'post',
+        body: JSON.stringify({
+        messages: messages
+        }),
+           headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+    .then(res => {
+        messages.push(res);
+        displayMessage(res.content, 'AI');
+    })
+})
+
+function displayMessage(content, by) {
+    document.querySelector('.chat-area').innerHTML += `
+     <div class="chat by-${by}">
+                    <div class="avatar">
+                    ${by}
+                        
+                    </div>
+                    <div class="chat-content">
+                        ${content}
+                    </div>
+                 </div> `
+
+}
